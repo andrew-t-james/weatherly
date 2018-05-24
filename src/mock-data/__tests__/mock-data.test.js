e75771f55566d4e1985b105991f025fbeef83fa6
Andrew James <ajames20@gmail.com>
2018-05-24 14:32:52 -0600

    Add test coverage for mock-data current location

--- src/mock-data/__tests__/mock-data.test.js
+++ src/mock-data/__tests__/mock-data.test.js
@@ -1,6 +1,14 @@
-import { data, dailyForecast } from '../mock-data.js';
+import { data, currentLocation, dailyForecast } from '../mock-data.js';
 
 describe('Mock data unit tests', () => {
+  test('returns weather for current location', () => {
+    const actual = currentLocation();
+
+    const expected =   data.current_observation;
+
+    expect(actual).toEqual(expected);
+  });
+
   test('returns daily for 10 days', () => {
     const actual = dailyForecast();
 
