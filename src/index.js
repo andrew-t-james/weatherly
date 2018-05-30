8d4823e97bb179cbe7694e51eb64f6dd6721391d
Andrew James <ajames20@gmail.com>
2018-05-30 13:04:13 -0600

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App.js';
 import './css/main.css';
 import './css/weather-icons.css';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
